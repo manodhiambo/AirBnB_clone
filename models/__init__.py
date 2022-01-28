f23bc8adc7b241799f0b8915b22ce9089bc7db10
felix abongo <aggreyfelix@yahoo.com>
2022-01-28 02:34:05 -0800

    bug fix init

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """
-initialize the models package
+__init__ dunder method for the models directory
 """
-=======
-""" create a unique FileStorage instance for your application """
->>>>>>> 5d10a342d89ab5775900c4d6b39a51e8a13a1f1b
-
 from models.engine.file_storage import FileStorage
+
 
 storage = FileStorage()
 storage.reload()
